c71043bfd36abfcd33036d577a81b7117528fbe2
soupurtion <soupurtion@hotmail.com>
2024-03-04 22:57:35 -0700

    Raspberry Pi data

--- Scripts/cpu_cycles.py
+++ Scripts/cpu_cycles.py
@@ -5,7 +5,7 @@ import datetime
 N = 100000
 
 
-outputFile = open('cpu_secp256r1_MiniPC.csv', 'w')
+outputFile = open('cpu_secp256r1_RPi.csv', 'w')
 outputFile.write("CPU Cycles" + ';')
 outputFile.write("CPU instructions" + ',' + '\n')
 
@@ -17,8 +17,7 @@ def terminal(cmd):
 
 
 for i in range(N):
-	#raw = terminal('sudo perf stat -e cycles ../venv/bin/python x25519.py 2>&1 | grep "cycles"').strip().split()
-	raw = terminal('sudo perf stat -e cycles,instructions ../venv/bin/python secp2561.py 2>&1 | grep -E "cycles|instructions"').strip().split()
+	raw = terminal('sudo perf stat -e cycles,instructions ~/PQC/venv/bin/python secp2561.py 2>&1 | grep -E "cycles|instructions"').strip().split()
 	cycle = raw[0]
 	instruct = raw[2]
 	outputFile.write(str(cycle) + ';')
@@ -27,18 +26,27 @@ for i in range(N):
 	
 
 
-outputFile = open('cpu_kyber512_MiniPC.csv', 'w')
+outputFile = open('cpu_kyber512_Rpi.csv', 'w')
 outputFile.write("CPU Cycles" + ';')
 outputFile.write("CPU instructions" + ',' + '\n')
 
 
-
-
+for i in range(N):
+	raw = terminal('sudo perf stat -e cycles,instructions ~/PQC/venv/bin/python kem.py 2>&1 | grep -E "cycles|instructions"').strip().split()
+	cycle = raw[0]
+	instruct = raw[2]
+	outputFile.write(str(cycle) + ';')
+	outputFile.write(str(instruct) + '\n')
+	print(i)
+	
+	
+outputFile = open('cpu_x25519_Rpi.csv', 'w')
+outputFile.write("CPU Cycles" + ';')
+outputFile.write("CPU instructions" + ',' + '\n')
 
 
 for i in range(N):
-	#raw = terminal('sudo perf stat -e cycles ../venv/bin/python x25519.py 2>&1 | grep "cycles"').strip().split()
-	raw = terminal('sudo perf stat -e cycles,instructions ../venv/bin/python kem.py 2>&1 | grep -E "cycles|instructions"').strip().split()
+	raw = terminal('sudo perf stat -e cycles,instructions ~/PQC/venv/bin/python x25519.py 2>&1 | grep -E "cycles|instructions"').strip().split()
 	cycle = raw[0]
 	instruct = raw[2]
 	outputFile.write(str(cycle) + ';')
